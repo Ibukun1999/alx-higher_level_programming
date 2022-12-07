65f648036e48cc8b4e20defd94a636ae85e7df61
Ibukun1999 <110995304+Ibukun1999@users.noreply.github.com>
2022-12-07 21:23:43 +0100

    Update 2-args.py

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,15 +1,9 @@
 #!/usr/bin/python3
 
 if __name__ == "__main__":
-    """Print the number of and list of arguments."""
-    import sys
+    """Print the sum of 1 and 2."""
+    from add_0 import add
 
-    count = len(sys.argv) - 1
-    if count == 0:
-        print("0 arguments.")
-    elif count == 1:
-        print("1 argument:")
-    else:
-        print("{} arguments:".format(count))
-    for i in range(count):
-        print("{}: {}".format(i + 1, sys.argv[i + 1]))
+    a = 1
+    b = 2
+    print("{} + {} = {}".format(a, b, add(a, b)))
